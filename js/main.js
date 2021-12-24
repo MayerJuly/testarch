52ead3e8acf5e0b19784887845d9306dd4056aae
MayerJuly <73027797+MayerJuly@users.noreply.github.com>
2021-12-25 02:58:02 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(function(){
+ // Range slider
  $(window).on("load", function(){
   let range = $("#range").attr("value");
   $("#demo").html(range);
@@ -10,6 +11,8 @@ $(function(){
    $(".slide").css("width", slideWidth + "%");
   });
  });
+
+ // Quiz
  let quizHeight = ["717px","792px","857px","772px","972px"],
      i = 0,
      page = 1;
@@ -17,11 +20,9 @@ $(function(){
  let inputs = $('.quiz input[required]')
 
  quiz.find('.page').fadeOut(0);
- 
+
  inputs.removeAttr("required");
  function checkInputs(page) {
-  if(page===1){
-  }
   if ($('.page'+page+' input[type=radio]:checked').length > 0 && page!==3 && page!==2 && page!==6 && page!==5)
    return true;
   else if (page === 2 && checkInputsText(page) && $('.page' + page + ' input[type=radio]:checked').length > 0)
@@ -33,9 +34,11 @@ $(function(){
   else if(page ===6 && checkInputsText(page) && $('.page'+ page +' input[type=radio]:checked').length >0 && $('.page'+ page +' input[type=checkbox]:checked').length >0 )
    return true;
   else{
+   $('.page'+page+' .button-next').addClass('err');
  return false;
   }
  }
+
 
  function checkInputsText(page){
   let flag = 0;
@@ -52,6 +55,7 @@ $(function(){
  $('.button-next').on('click', function(e){
   e.preventDefault();
   if(!$(e.target).hasClass('button-end') && !$(e.target).parent().hasClass('button-end') && checkInputs(page)){
+   if($('.button-next').hasClass('err')) $('.button-next').removeClass('err');
    $(e.target).closest('.page').fadeOut(600);
   quiz.css('min-height', quizHeight[i]);
    i++;
@@ -90,6 +94,51 @@ $(function(){
   $(e.target).closest('.page').next().fadeIn(600);
 
  });
+
+
+// Tabs
+
+  let tab = $('#tabs .tabs__items > div');
+  tab.hide().filter(':last').show();
+
+  // Клики по вкладкам.
+  $('#tabs .tabs__nav a').click(function(e){
+   if($(e.target).attr('id')==='tab2') {
+    $('.front__tabs-bg').css('background', 'linear-gradient(180deg, #EDEDED 33.77%, #DBDBDB 56.96%)');
+   }
+   else{
+    $('.front__tabs-bg').css('background', '#EAEAEA');
+   }
+   tab.hide();
+  tab.filter(this.hash).show();
+  $('#tabs .tabs__nav a').removeClass('active');
+  $(this).addClass('active');
+  return false;
+ }).filter(':last').click();
+
+
+
+ $('.features__content').on('click',function(e){
+  $(e.target).closest('.features__content').toggleClass('animated');
+   $(e.target).closest('.features__content').toggleClass('opened');
+ })
+
+ // Technology tabs
+
+ let tab_2 = $('#tabs_2 .tabs__items > div');
+ tab_2.hide().filter(':first').show();
+
+ // Клики по вкладкам.
+ $('#tabs_2 .tabs__nav a').click(function(e){
+  tab_2.hide();
+  tab_2.filter(this.hash).show();
+  $('#tabs_2 .tabs__nav a').removeClass('active');
+  $(this).addClass('active');
+  return false;
+ }).filter(':first').click();
+
+
+
 });
 
 
